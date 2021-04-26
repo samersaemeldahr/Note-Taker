7268beee3219ad5ee9f4a3bcda79bf4b3aae78a0
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-04-26 16:53:37 +0300

    require database json

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,36 @@
-const express = require('express');
-
+const express = require("express");
+const nodemon = require("nodemon");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db.json");
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
-// require('./public/assets/js/index')(app);
-require('./public/assets/js/htmlRoutes')(app);
+
+require('./public/assets/js/index')(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+
+// If no matching route is found default to home
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
 
 app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
+    console.log(`App listening on PORT: ${PORT}`);
 });
