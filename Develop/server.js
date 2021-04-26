5b0fb79dd5ddc1982a6f0dad3eefde5de4390e02
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-04-26 22:23:48 +0300

    get notes from database

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const database = require("./db/db.json");
-const uniqueid = require("uniqeid")
+// const uniqueid = require("uniqeid")
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -12,14 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+app.get('/api/notes', (req, res) => res.json(database));
 
 
 
